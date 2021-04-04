fe283b55615981a2b8358de509c2735ac6e726d3
Lysindr <vetercwg@gmail.com>
2021-04-04 20:29:11 +0300

    add pages

---
+++ components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+	<nav class="navigation">
+		<NuxtLink to="/">Home</NuxtLink>
+		<NuxtLink to="/about-us">About us</NuxtLink>
+		<NuxtLink to="/contact-us">Contact us</NuxtLink>
+
+		<nuxt-link to="/about-us" no-prefetch>About (not prefetched)</nuxt-link>
+	</nav>
+</template>
+
+<style>
+	.navigation {
+		margin: 15px 0;
+		padding: 15px 10px;
+	}
+
+	.navigation a {
+		display: inline-block;
+		margin: 0 10px;
+	}
+</style>
--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        Contact
-      </h1>
-      <div class="links">
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="/contact-us">Contact us</NuxtLink>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div>
+			<Logo />
+
+			<Navigation />
+
+			<h1 class="title">
+				Contact Us
+			</h1>
+		</div>		
+	</div>
 </template>
 
 <script>
-export default {}
+	export default {}
 </script>
